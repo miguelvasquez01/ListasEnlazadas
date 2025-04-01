0fc5e54a1ad149dd76c9da72e1ecf2267e2ad418
Miguel Vasquez <vgmiguel16@gmail.com>
2025-04-01 17:41:53 -0500

    Primeros métodos lista enlazada doble

--- src/main/java/com/listas_enlazadas/ListaDoblementeEnlazada/ListaDoble.java
+++ src/main/java/com/listas_enlazadas/ListaDoblementeEnlazada/ListaDoble.java
@@ -5,4 +5,37 @@ public class ListaDoble <T> {
     private Nodo<T> inicial;
     private Nodo<T> ultimo;
     private int tamano = 0;
+
+    public void mostrar() {
+
+        if (inicial == null || ultimo == null) {
+            System.out.println("Lista vacía");
+            return;
+        }
+
+        Nodo<T> nodoRecorrer = inicial;
+        while (nodoRecorrer != null) { //nodoRecorrer != ultimo
+
+            System.out.print(nodoRecorrer.getValor() + ", ");
+            nodoRecorrer = nodoRecorrer.getSiguiente();
+        }
+        // System.out.print(ultimo.getValor());
+        System.out.println();
+    }
+
+    public void agregarFinal(T valor) {
+
+        Nodo<T> nodo = new Nodo<T>(valor);
+
+        if (inicial == null || ultimo == null) {
+            inicial = nodo;
+            ultimo = nodo;
+        } else {
+
+            ultimo.setSiguiente(nodo);
+            nodo.setAnterior(ultimo);
+            ultimo = nodo;
+        }
+        tamano++;
+    }
 }
--- src/main/java/com/listas_enlazadas/ListaDoblementeEnlazada/Main.java
+++ src/main/java/com/listas_enlazadas/ListaDoblementeEnlazada/Main.java
@@ -4,5 +4,12 @@ public class Main {
     
     public static void main(String[] args) {
         
+        ListaDoble<Integer> lista = new ListaDoble<>();
+        lista.agregarFinal(21);
+        lista.agregarFinal(22);
+        lista.agregarFinal(23);
+        lista.agregarFinal(24);
+
+        lista.mostrar();
     }
 }
